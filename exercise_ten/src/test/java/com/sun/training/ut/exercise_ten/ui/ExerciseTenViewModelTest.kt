4a097a5e50fd3c1f72fb96bf9107a482bf8ad9bd
oNhamManhTuyen <nham.manh.tuyen@framgia.com>
2021-04-06 15:23:05 +0700

    Unit Test exercise 10 - TuyenNM

--- exercise_ten/src/test/java/com/sun/training/ut/exercise_ten/ui/ExerciseTenViewModelTest.kt
+++ exercise_ten/src/test/java/com/sun/training/ut/exercise_ten/ui/ExerciseTenViewModelTest.kt
@@ -3,6 +3,7 @@ package com.sun.training.ut.exercise_ten.ui
 import android.content.res.Resources
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.exercise_ten.R
+import com.sun.training.ut.exercise_ten.data.model.MemberClassType
 import com.sun.training.ut.exercise_ten.domain.business.PaymentAmountPointBusiness
 import org.junit.Assert
 import org.junit.Before
@@ -38,10 +39,24 @@ class ExerciseTenViewModelTest {
     }
 
     @Test
+    fun testDiscountCalculation_blackClass_subtotal1000_equal0() {
+        viewModel.updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_black))
+
+        Assert.assertEquals(0.0, viewModel.discountCalculation(1000.0), 0.0)
+    }
+
+    @Test
     fun testDiscountCalculation_blackClass_subtotal3000_equal150() {
         viewModel.updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_black))
 
         Assert.assertEquals(150.0, viewModel.discountCalculation(PaymentAmountPointBusiness.PAYMENT_3K), 0.0)
+    }
+
+    @Test
+    fun testDiscountCalculation_blackClass_subtotal4000_equal200() {
+        viewModel.updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_black))
+
+        Assert.assertEquals(200.0, viewModel.discountCalculation(4000.0), 0.0)
     }
 
     @Test
@@ -52,10 +67,32 @@ class ExerciseTenViewModelTest {
     }
 
     @Test
+    fun testDiscountCalculation_blackClass_subtotal8000_equal560() {
+        viewModel.updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_black))
+
+        Assert.assertEquals(560.0, viewModel.discountCalculation(8000.0), 0.0)
+    }
+
+
+    @Test
     fun testDiscountCalculation_blackClass_subtotal10000_equal1500() {
         viewModel.updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_black))
 
         Assert.assertEquals(1500.0, viewModel.discountCalculation(PaymentAmountPointBusiness.PAYMENT_10K), 0.0)
+    }
+
+    @Test
+    fun testDiscountCalculation_blackClass_subtotal11000_equal1500() {
+        viewModel.updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_black))
+
+        Assert.assertEquals(1650.0, viewModel.discountCalculation(11000.0), 0.0)
+    }
+
+    @Test
+    fun testDiscountCalculation_goldClass_subtotal1000_equal0() {
+        viewModel.updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_gold))
+
+        Assert.assertEquals(0.0, viewModel.discountCalculation(1000.0), 0.0)
     }
 
     @Test
@@ -66,10 +103,24 @@ class ExerciseTenViewModelTest {
     }
 
     @Test
+    fun testDiscountCalculation_goldClass_subtotal4000_equal120() {
+        viewModel.updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_gold))
+
+        Assert.assertEquals(120.0, viewModel.discountCalculation(4000.0), 0.0)
+    }
+
+    @Test
     fun testDiscountCalculation_goldClass_subtotal5000_equal250() {
         viewModel.updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_gold))
 
         Assert.assertEquals(250.0, viewModel.discountCalculation(PaymentAmountPointBusiness.PAYMENT_5K), 0.0)
+    }
+
+    @Test
+    fun testDiscountCalculation_goldClass_subtotal8000_equal250() {
+        viewModel.updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_gold))
+
+        Assert.assertEquals(400.0, viewModel.discountCalculation(8000.0), 0.0)
     }
 
     @Test
@@ -79,6 +130,40 @@ class ExerciseTenViewModelTest {
         Assert.assertEquals(1000.0, viewModel.discountCalculation(PaymentAmountPointBusiness.PAYMENT_10K), 0.0)
     }
 
+    @Test
+    fun testDiscountCalculation_goldClass_subtotal11000_equal1000() {
+        viewModel.updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_gold))
+
+        Assert.assertEquals(1100.0, viewModel.discountCalculation(11000.0), 0.0)
+    }
+
+    @Test
+    fun testDiscountCalculation_silverClass_subtotal1000_equal30() {
+        viewModel.updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_silver))
+
+        Assert.assertEquals(0.0, viewModel.discountCalculation(1000.0), 0.0)
+    }
+
+    @Test
+    fun testDiscountCalculation_silverClass_subtotal4000_equal30() {
+        viewModel.updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_silver))
+
+        Assert.assertEquals(40.0, viewModel.discountCalculation(4000.0), 0.0)
+    }
+
+    @Test
+    fun testDiscountCalculation_silverClass_subtotal8000_equal30() {
+        viewModel.updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_silver))
+
+        Assert.assertEquals(160.0, viewModel.discountCalculation(8000.0), 0.0)
+    }
+
+    @Test
+    fun testDiscountCalculation_silverClass_subtotal11000_equal30() {
+        viewModel.updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_silver))
+
+        Assert.assertEquals(440.0, viewModel.discountCalculation(11000.0), 0.0)
+    }
 
     @Test
     fun testDiscountCalculation_silverClass_subtotal3000_equal30() {
@@ -109,8 +194,18 @@ class ExerciseTenViewModelTest {
     }
 
     @Test
+    fun giftAccepted_subTotal1000_ReturnFalse() {
+        Assert.assertEquals(false, viewModel.giftAccepted(1000.0))
+    }
+
+    @Test
     fun giftAccepted_subTotal3000_ReturnFalse() {
         Assert.assertEquals(false, viewModel.giftAccepted(PaymentAmountPointBusiness.PAYMENT_3K))
+    }
+
+    @Test
+    fun giftAccepted_subTotal4000_ReturnFalse() {
+        Assert.assertEquals(false, viewModel.giftAccepted(4000.0))
     }
 
     @Test
@@ -119,7 +214,383 @@ class ExerciseTenViewModelTest {
     }
 
     @Test
+    fun giftAccepted_subTotal8000_ReturnFalse() {
+        Assert.assertEquals(false, viewModel.giftAccepted(8000.0))
+    }
+
+    @Test
     fun giftAccepted_subTotal10000_ReturnTrue() {
         Assert.assertEquals(true, viewModel.giftAccepted(PaymentAmountPointBusiness.PAYMENT_10K))
+    }
+
+    @Test
+    fun giftAccepted_subTotal11000_ReturnFalse() {
+        Assert.assertEquals(false, viewModel.giftAccepted(11000.0))
+    }
+
+    @Test
+    fun updateMemberClassType_unknown_ReturnUnknownClass() {
+        viewModel.updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_unknown))
+
+        Assert.assertEquals(MemberClassType.UNKNOWN_CLASS, viewModel.user.value?.classType)
+    }
+
+    @Test
+    fun updateMemberClassType_silver_ReturnSilverClass() {
+        viewModel.updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_silver))
+
+        Assert.assertEquals(MemberClassType.SILVER_CLASS, viewModel.user.value?.classType)
+    }
+
+    @Test
+    fun updateMemberClassType_gold_ReturnSilverClass() {
+        viewModel.updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_gold))
+
+        Assert.assertEquals(MemberClassType.GOLD_CLASS, viewModel.user.value?.classType)
+    }
+
+    @Test
+    fun updateMemberClassType_black_ReturnSilverClass() {
+        viewModel.updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_black))
+
+        Assert.assertEquals(MemberClassType.BLACK_CLASS, viewModel.user.value?.classType)
+    }
+
+    @Test
+    fun printInvoice_silver_subTotal1000() {
+        with(viewModel) {
+            updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_silver))
+            subTotal.value = "1000"
+            printInvoice()
+
+            val invoice = invoice.value
+
+            Assert.assertEquals(0.0, invoice?.discount)
+            Assert.assertEquals(1000.0, invoice?.subTotal)
+            Assert.assertEquals(1000.0, invoice?.total)
+            Assert.assertEquals(false, invoice?.giftAccepted)
+        }
+    }
+
+    @Test
+    fun printInvoice_silver_subTotal3000() {
+        with(viewModel) {
+            updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_silver))
+            subTotal.value = "3000"
+            printInvoice()
+
+            val invoice = invoice.value
+
+            Assert.assertEquals(30.0, invoice?.discount)
+            Assert.assertEquals(3000.0, invoice?.subTotal)
+            Assert.assertEquals(2970.0, invoice?.total)
+            Assert.assertEquals(false, invoice?.giftAccepted)
+        }
+    }
+
+    @Test
+    fun printInvoice_silver_subTotal4000() {
+        with(viewModel) {
+            updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_silver))
+            subTotal.value = "4000"
+            printInvoice()
+
+            val invoice = invoice.value
+
+            Assert.assertEquals(40.0, invoice?.discount)
+            Assert.assertEquals(4000.0, invoice?.subTotal)
+            Assert.assertEquals(3960.0, invoice?.total)
+            Assert.assertEquals(false, invoice?.giftAccepted)
+        }
+    }
+
+    @Test
+    fun printInvoice_silver_subTotal5000() {
+        with(viewModel) {
+            updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_silver))
+            subTotal.value = "5000"
+            printInvoice()
+
+            val invoice = invoice.value
+
+            Assert.assertEquals(100.0, invoice?.discount)
+            Assert.assertEquals(5000.0, invoice?.subTotal)
+            Assert.assertEquals(4900.0, invoice?.total)
+            Assert.assertEquals(true, invoice?.giftAccepted)
+        }
+    }
+
+    @Test
+    fun printInvoice_silver_subTotal8000() {
+        with(viewModel) {
+            updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_silver))
+            subTotal.value = "8000"
+            printInvoice()
+
+            val invoice = invoice.value
+
+            Assert.assertEquals(160.0, invoice?.discount)
+            Assert.assertEquals(8000.0, invoice?.subTotal)
+            Assert.assertEquals(7840.0, invoice?.total)
+            Assert.assertEquals(false, invoice?.giftAccepted)
+        }
+    }
+
+    @Test
+    fun printInvoice_silver_subTotal10000() {
+        with(viewModel) {
+            updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_silver))
+            subTotal.value = "10000"
+            printInvoice()
+
+            val invoice = invoice.value
+
+            Assert.assertEquals(400.0, invoice?.discount)
+            Assert.assertEquals(10000.0, invoice?.subTotal)
+            Assert.assertEquals(9600.0, invoice?.total)
+            Assert.assertEquals(true, invoice?.giftAccepted)
+        }
+    }
+
+    @Test
+    fun printInvoice_silver_subTotal11000() {
+        with(viewModel) {
+            updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_silver))
+            subTotal.value = "11000"
+            printInvoice()
+
+            val invoice = invoice.value
+
+            Assert.assertEquals(440.0, invoice?.discount)
+            Assert.assertEquals(11000.0, invoice?.subTotal)
+            Assert.assertEquals(10560.0, invoice?.total)
+            Assert.assertEquals(false, invoice?.giftAccepted)
+        }
+    }
+
+
+    @Test
+    fun printInvoice_gold_subTotal1000() {
+        with(viewModel) {
+            updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_gold))
+            subTotal.value = "1000"
+            printInvoice()
+
+            val invoice = invoice.value
+
+            Assert.assertEquals(0.0, invoice?.discount)
+            Assert.assertEquals(1000.0, invoice?.subTotal)
+            Assert.assertEquals(1000.0, invoice?.total)
+            Assert.assertEquals(false, invoice?.giftAccepted)
+        }
+    }
+
+    @Test
+    fun printInvoice_gold_subTotal3000() {
+        with(viewModel) {
+            updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_gold))
+            subTotal.value = "3000"
+            printInvoice()
+
+            val invoice = invoice.value
+
+            Assert.assertEquals(90.0, invoice?.discount)
+            Assert.assertEquals(3000.0, invoice?.subTotal)
+            Assert.assertEquals(2910.0, invoice?.total)
+            Assert.assertEquals(false, invoice?.giftAccepted)
+        }
+    }
+
+    @Test
+    fun printInvoice_gold_subTotal4000() {
+        with(viewModel) {
+            updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_gold))
+            subTotal.value = "4000"
+            printInvoice()
+
+            val invoice = invoice.value
+
+            Assert.assertEquals(120.0, invoice?.discount)
+            Assert.assertEquals(4000.0, invoice?.subTotal)
+            Assert.assertEquals(3880.0, invoice?.total)
+            Assert.assertEquals(false, invoice?.giftAccepted)
+        }
+    }
+
+    @Test
+    fun printInvoice_gold_subTotal5000() {
+        with(viewModel) {
+            updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_gold))
+            subTotal.value = "5000"
+            printInvoice()
+
+            val invoice = invoice.value
+
+            Assert.assertEquals(250.0, invoice?.discount)
+            Assert.assertEquals(5000.0, invoice?.subTotal)
+            Assert.assertEquals(4750.0, invoice?.total)
+            Assert.assertEquals(true, invoice?.giftAccepted)
+        }
+    }
+
+    @Test
+    fun printInvoice_gold_subTotal8000() {
+        with(viewModel) {
+            updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_gold))
+            subTotal.value = "8000"
+            printInvoice()
+
+            val invoice = invoice.value
+
+            Assert.assertEquals(400.0, invoice?.discount)
+            Assert.assertEquals(8000.0, invoice?.subTotal)
+            Assert.assertEquals(7600.0, invoice?.total)
+            Assert.assertEquals(false, invoice?.giftAccepted)
+        }
+    }
+
+    @Test
+    fun printInvoice_gold_subTotal10000() {
+        with(viewModel) {
+            updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_gold))
+            subTotal.value = "10000"
+            printInvoice()
+
+            val invoice = invoice.value
+
+            Assert.assertEquals(1000.0, invoice?.discount)
+            Assert.assertEquals(10000.0, invoice?.subTotal)
+            Assert.assertEquals(9000.0, invoice?.total)
+            Assert.assertEquals(true, invoice?.giftAccepted)
+        }
+    }
+
+    @Test
+    fun printInvoice_gold_subTotal11000() {
+        with(viewModel) {
+            updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_gold))
+            subTotal.value = "11000"
+            printInvoice()
+
+            val invoice = invoice.value
+
+            Assert.assertEquals(1100.0, invoice?.discount)
+            Assert.assertEquals(11000.0, invoice?.subTotal)
+            Assert.assertEquals(9900.0, invoice?.total)
+            Assert.assertEquals(false, invoice?.giftAccepted)
+        }
+    }
+
+
+    @Test
+    fun printInvoice_black_subTotal1000() {
+        with(viewModel) {
+            updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_black))
+            subTotal.value = "1000"
+            printInvoice()
+
+            val invoice = invoice.value
+
+            Assert.assertEquals(0.0, invoice?.discount)
+            Assert.assertEquals(1000.0, invoice?.subTotal)
+            Assert.assertEquals(1000.0, invoice?.total)
+            Assert.assertEquals(false, invoice?.giftAccepted)
+        }
+    }
+
+    @Test
+    fun printInvoice_black_subTotal3000() {
+        with(viewModel) {
+            updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_black))
+            subTotal.value = "3000"
+            printInvoice()
+
+            val invoice = invoice.value
+
+            Assert.assertEquals(150.0, invoice?.discount)
+            Assert.assertEquals(3000.0, invoice?.subTotal)
+            Assert.assertEquals(2850.0, invoice?.total)
+            Assert.assertEquals(false, invoice?.giftAccepted)
+        }
+    }
+
+    @Test
+    fun printInvoice_black_subTotal4000() {
+        with(viewModel) {
+            updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_black))
+            subTotal.value = "4000"
+            printInvoice()
+
+            val invoice = invoice.value
+
+            Assert.assertEquals(200.0, invoice?.discount)
+            Assert.assertEquals(4000.0, invoice?.subTotal)
+            Assert.assertEquals(3800.0, invoice?.total)
+            Assert.assertEquals(false, invoice?.giftAccepted)
+        }
+    }
+
+    @Test
+    fun printInvoice_black_subTotal5000() {
+        with(viewModel) {
+            updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_black))
+            subTotal.value = "5000"
+            printInvoice()
+
+            val invoice = invoice.value
+
+            Assert.assertEquals(350.0, invoice?.discount ?: 0.0, 0.0001)
+            Assert.assertEquals(5000.0, invoice?.subTotal)
+            Assert.assertEquals(4650.0, invoice?.total)
+            Assert.assertEquals(true, invoice?.giftAccepted)
+        }
+    }
+
+    @Test
+    fun printInvoice_black_subTotal8000() {
+        with(viewModel) {
+            updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_black))
+            subTotal.value = "8000"
+            printInvoice()
+
+            val invoice = invoice.value
+
+            Assert.assertEquals(560.0, invoice?.discount)
+            Assert.assertEquals(8000.0, invoice?.subTotal)
+            Assert.assertEquals(7440.0, invoice?.total)
+            Assert.assertEquals(false, invoice?.giftAccepted)
+        }
+    }
+
+    @Test
+    fun printInvoice_black_subTotal10000() {
+        with(viewModel) {
+            updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_black))
+            subTotal.value = "10000"
+            printInvoice()
+
+            val invoice = invoice.value
+
+            Assert.assertEquals(1500.0, invoice?.discount)
+            Assert.assertEquals(10000.0, invoice?.subTotal)
+            Assert.assertEquals(8500.0, invoice?.total)
+            Assert.assertEquals(true, invoice?.giftAccepted)
+        }
+    }
+
+    @Test
+    fun printInvoice_black_subTotal11000() {
+        with(viewModel) {
+            updateMemberClassType(mockResources.getString(R.string.ex_10_class_type_black))
+            subTotal.value = "11000"
+            printInvoice()
+
+            val invoice = invoice.value
+
+            Assert.assertEquals(1650.0, invoice?.discount)
+            Assert.assertEquals(11000.0, invoice?.subTotal)
+            Assert.assertEquals(9350.0, invoice?.total)
+            Assert.assertEquals(false, invoice?.giftAccepted)
+        }
     }
 }
