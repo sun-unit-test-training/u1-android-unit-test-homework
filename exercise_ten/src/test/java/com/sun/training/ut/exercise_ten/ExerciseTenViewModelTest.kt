e533d6cff7264b832a5b35d1dbcf1ace38a37544
Nguyen Ngoc Son <nguyen.ngoc.son-c@sun-asterisk.com>
2021-04-12 21:59:32 +0700

    Fix comment ex10

--- exercise_ten/src/test/java/com/sun/training/ut/exercise_ten/ExerciseTenViewModelTest.kt
+++ exercise_ten/src/test/java/com/sun/training/ut/exercise_ten/ExerciseTenViewModelTest.kt
@@ -1,9 +1,10 @@
 package com.sun.training.ut.exercise_ten
 
 import android.content.Context
-import android.os.Build
+import android.content.res.Resources
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.exercise_ten.R
+import com.sun.training.ut.exercise_ten.data.model.MemberClassType
 import com.sun.training.ut.exercise_ten.ui.ExerciseTenActivity
 import com.sun.training.ut.exercise_ten.ui.ExerciseTenViewModel
 import org.junit.Assert.assertEquals
@@ -12,13 +13,15 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestRule
 import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
+import org.mockito.junit.MockitoJUnitRunner
 import org.robolectric.Robolectric
-import org.robolectric.RobolectricTestRunner
-import org.robolectric.annotation.Config
+import java.lang.Exception
+import kotlin.jvm.Throws
 
-//@RunWith(MockitoJUnitRunner::class)
-@RunWith(RobolectricTestRunner::class)
+@RunWith(MockitoJUnitRunner::class)
 class ExerciseTenViewModelTest {
 
     @get:Rule
@@ -26,13 +29,25 @@ class ExerciseTenViewModelTest {
 
     private lateinit var viewModel: ExerciseTenViewModel
 
+    @Mock
     private lateinit var context: Context
 
+    @Mock
+    private lateinit var resources: Resources
+
     @Before
+    @Throws(Exception::class)
     fun setUp() {
         MockitoAnnotations.initMocks(this)
-        val activity = Robolectric.buildActivity(ExerciseTenActivity::class.java)
-        context = activity.get().applicationContext
+        Mockito.`when`(context.resources).thenReturn(resources)
+        Mockito.`when`(resources.getString(R.string.ex_10_user_name_default))
+            .thenReturn("\"Bach Ngoc Hoai\"")
+        Mockito.`when`(resources.getString(R.string.ex_10_class_type_black)).thenReturn("Hạng Đen")
+        Mockito.`when`(resources.getString(R.string.ex_10_class_type_gold)).thenReturn("Hạng Vàng")
+        Mockito.`when`(resources.getString(R.string.ex_10_class_type_silver)).thenReturn("Hạng Bạc")
+        Mockito.`when`(resources.getString(R.string.ex_10_class_type_unknown))
+            .thenReturn("Không phải hội viên")
+
         viewModel = ExerciseTenViewModel(context.resources)
     }
 
@@ -244,5 +259,113 @@ class ExerciseTenViewModelTest {
         viewModel.printInvoice()
         assertEquals(true, viewModel.giftAccepted(subTotal))
         assertEquals(15, viewModel.invoice.value?.discount)
+    }
+
+    @Test
+    fun validate_noClass_below3000_discount0_noCoupon() {
+        val subTotal = 11000.0
+        viewModel.updateMemberClassType(context.getString(R.string.ex_10_class_type_black))
+        viewModel.discountCalculation(subTotal)
+        viewModel.printInvoice()
+        assertEquals(true, viewModel.giftAccepted(subTotal))
+        assertEquals(15, viewModel.invoice.value?.discount)
+    }
+
+    @Test
+    fun validate_noClass_below5000_discount0_noCoupon() {
+        val subTotal = 3000.0
+        viewModel.updateMemberClassType(context.getString(R.string.ex_10_class_type_unknown))
+        viewModel.discountCalculation(subTotal)
+        viewModel.printInvoice()
+        assertEquals(false, viewModel.giftAccepted(subTotal))
+        assertEquals(0, viewModel.invoice.value?.discount)
+    }
+
+    @Test
+    fun validate_noClass_5000_discount0_hasCoupon() {
+        val subTotal = 5000.0
+        viewModel.updateMemberClassType(context.getString(R.string.ex_10_class_type_unknown))
+        viewModel.discountCalculation(subTotal)
+        viewModel.printInvoice()
+        assertEquals(true, viewModel.giftAccepted(subTotal))
+        assertEquals(0, viewModel.invoice.value?.discount)
+    }
+
+    @Test
+    fun validate_noClass_above5000_discount0_hasCoupon() {
+        val subTotal = 6000.0
+        viewModel.updateMemberClassType(context.getString(R.string.ex_10_class_type_unknown))
+        viewModel.discountCalculation(subTotal)
+        viewModel.printInvoice()
+        assertEquals(true, viewModel.giftAccepted(subTotal))
+        assertEquals(0, viewModel.invoice.value?.discount)
+    }
+
+    @Test
+    fun validate_noClass_10000_discount0_hasCoupon() {
+        val subTotal = 10000.0
+        viewModel.updateMemberClassType(context.getString(R.string.ex_10_class_type_unknown))
+        viewModel.discountCalculation(subTotal)
+        viewModel.printInvoice()
+        assertEquals(true, viewModel.giftAccepted(subTotal))
+        assertEquals(0, viewModel.invoice.value?.discount)
+    }
+
+    @Test
+    fun validate_noClass_above10000_discount0_hasCoupon() {
+        val subTotal = 11000.0
+        viewModel.updateMemberClassType(context.getString(R.string.ex_10_class_type_unknown))
+        viewModel.discountCalculation(subTotal)
+        viewModel.printInvoice()
+        assertEquals(true, viewModel.giftAccepted(subTotal))
+        assertEquals(0, viewModel.invoice.value?.discount)
+    }
+
+    @Test
+    fun validate_gift_accepted_below5000_return_false() {
+        assertEquals(false, viewModel.giftAccepted(3000.0))
+    }
+
+    @Test
+    fun validate_gift_accepted_5000_return_true() {
+        assertEquals(true, viewModel.giftAccepted(5000.0))
+    }
+
+    @Test
+    fun validate_gift_accepted_above5000_return_true() {
+        assertEquals(true, viewModel.giftAccepted(6000.0))
+    }
+
+    @Test
+    fun validate_gift_accepted_10000_return_true() {
+        assertEquals(true, viewModel.giftAccepted(10000.0))
+    }
+    @Test
+    fun validate_gift_accepted_above10000_return_true() {
+        assertEquals(true, viewModel.giftAccepted(11000.0))
+    }
+
+    @Test
+    fun validate_update_memberClass_blackClass() {
+        viewModel.updateMemberClassType(context.getString(R.string.ex_10_class_type_black))
+        assertEquals(viewModel.user.value?.classType, MemberClassType.BLACK_CLASS)
+    }
+
+    @Test
+    fun validate_update_memberClass_goldClass() {
+        viewModel.updateMemberClassType(context.getString(R.string.ex_10_class_type_gold))
+        assertEquals(viewModel.user.value?.classType, MemberClassType.GOLD_CLASS)
+    }
+
+    @Test
+    fun validate_update_memberClass_silverClass() {
+        viewModel.updateMemberClassType(context.getString(R.string.ex_10_class_type_silver))
+        assertEquals(viewModel.user.value?.classType, MemberClassType.SILVER_CLASS)
+    }
+
+    @Test
+    fun validate_update_memberClass_unknow() {
+        viewModel.updateMemberClassType("")
+        assertEquals(viewModel.user.value?.classType, MemberClassType.UNKNOWN_CLASS)
     }
 }
